d3906d06960c5a12b8e9df962be409b89592b63e
Emre Berber <brbr.emre@gmail.com>
2017-09-14 22:01:32 +0300

    devexpress2

---
+++ 065-Devexpress2/065-Devexpress2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace _065_Devexpress2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBoxControl1 = new DevExpress.XtraEditors.ListBoxControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.checkEdit1 = new DevExpress.XtraEditors.CheckEdit();
+            this.checkEdit2 = new DevExpress.XtraEditors.RatingControl();
+            this.ımageSlider1 = new DevExpress.XtraEditors.Controls.ImageSlider();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ımageSlider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxControl1
+            // 
+            this.listBoxControl1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listBoxControl1.Items.AddRange(new object[] {
+            "Bağcılar",
+            "Beşiktaş",
+            "Beylikdüzü",
+            "Esenyurt",
+            "Mecidiyeköy"});
+            this.listBoxControl1.Location = new System.Drawing.Point(51, 95);
+            this.listBoxControl1.Name = "listBoxControl1";
+            this.listBoxControl1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxControl1.Size = new System.Drawing.Size(111, 137);
+            this.listBoxControl1.SortOrder = System.Windows.Forms.SortOrder.Ascending;
+            this.listBoxControl1.TabIndex = 0;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(51, 56);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(111, 33);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "ekle";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(177, 56);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(111, 33);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "texteditten";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(51, 30);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(237, 20);
+            this.textEdit1.TabIndex = 3;
+            // 
+            // checkEdit1
+            // 
+            this.checkEdit1.Location = new System.Drawing.Point(177, 104);
+            this.checkEdit1.Name = "checkEdit1";
+            this.checkEdit1.Properties.Caption = "Ortaköy";
+            this.checkEdit1.Size = new System.Drawing.Size(92, 19);
+            this.checkEdit1.TabIndex = 4;
+            // 
+            // checkEdit2
+            // 
+            this.checkEdit2.EditValue = false;
+            this.checkEdit2.Location = new System.Drawing.Point(177, 138);
+            this.checkEdit2.Name = "checkEdit2";
+            this.checkEdit2.Rating = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.checkEdit2.Size = new System.Drawing.Size(87, 16);
+            this.checkEdit2.TabIndex = 5;
+            this.checkEdit2.Text = "Ortaköy";
+            // 
+            // ımageSlider1
+            // 
+            this.ımageSlider1.CurrentImageIndex = 0;
+            this.ımageSlider1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ımageSlider1.Images.Add(((System.Drawing.Image)(resources.GetObject("ımageSlider1.Images"))));
+            this.ımageSlider1.Images.Add(((System.Drawing.Image)(resources.GetObject("ımageSlider1.Images1"))));
+            this.ımageSlider1.Images.Add(((System.Drawing.Image)(resources.GetObject("ımageSlider1.Images2"))));
+            this.ımageSlider1.Location = new System.Drawing.Point(341, 33);
+            this.ımageSlider1.Name = "ımageSlider1";
+            this.ımageSlider1.Size = new System.Drawing.Size(330, 199);
+            this.ımageSlider1.TabIndex = 6;
+            this.ımageSlider1.Text = "ımageSlider1";
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
+            this.pictureEdit1.Location = new System.Drawing.Point(51, 238);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pictureEdit1.Size = new System.Drawing.Size(259, 138);
+            this.pictureEdit1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 388);
+            this.Controls.Add(this.pictureEdit1);
+            this.Controls.Add(this.ımageSlider1);
+            this.Controls.Add(this.checkEdit1);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.listBoxControl1);
+            this.Controls.Add(this.checkEdit2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.listBoxControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ımageSlider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.ListBoxControl listBoxControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.CheckEdit checkEdit1;
+        private DevExpress.XtraEditors.RatingControl checkEdit2;
+        private DevExpress.XtraEditors.Controls.ImageSlider ımageSlider1;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+    }
+}
+
---
+++ 065-Devexpress2/065-Devexpress2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _065_Devexpress2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBoxControl1.Items.Add("Pendik");
+            listBoxControl1.Items.Add("Üsküdar");
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            listBoxControl1.Items.Add("Tuzla");
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            listBoxControl1.Items.Add(textEdit1.Text);
+        }
+    }
+}
